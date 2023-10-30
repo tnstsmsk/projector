9a6e33ffdbb8d9257264e088d5c5917d8fd8f470
tnstsmsk <148859147+tnstsmsk@users.noreply.github.com>
2023-10-30 18:11:36 +0200

    Add files via upload

---
+++ hw9_2.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+x = np.arange(15, dtype=np.int64).reshape(3, 5)
+x[1:, ::2] = -99
+x
+
+x.max(axis=1)
+
+rng = np.random.default_rng()
+samples = rng.normal(size=2500)
+
+print(samples)
