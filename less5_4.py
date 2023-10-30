b90991e80331260826ac8939b41e6a8409681aaf
tnstsmsk <omelkaramelka@gmail.com>
2023-10-30 12:29:03 +0200

    updated Subway Route Planner

--- less5_4.py
+++ less5_4.py
@@ -9,26 +9,33 @@ subway_system = {
 
 from collections import deque
 
-def shortest_route(subway_system, start_station, end_station):
+
+def person_is_seller(station):
+    return station[-1] == 'F'
+
+
+def search(station):
     search_queue = deque()
-    search_queue.append((start_station, None))
+    search_queue.append((station, None))
     searched = {}
 
     while search_queue:
+
         station, prev = search_queue.popleft()
+
         if station not in searched:
             searched[station] = prev
 
-        if end_station:
-            result = []
-            while station is not None:
-                result.append(station)
-                station = searched[station]
-            return result[::-1]
-        else:
-            search_queue += [(neighbor, station) for neighbor in subway_system[station]]
+            if person_is_seller(station):
+                result = []
+                while station is not None:
+                    result.append(station)
+                    station = searched[station]
+                return result[::-1]
+            else:
+                search_queue += [(neighbor, station) for neighbor in subway_system[station]]
+
     return []
 
 
-
-print(shortest_route(subway_system, 'Station A', 'Station F'))
+print('Shortest_route:', search('Station A'))
