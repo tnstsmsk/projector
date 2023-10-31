7ab9bbc453be8c384c0aacc2ef9512b3850aa372
tnstsmsk <omelkaramelka@gmail.com>
2023-10-31 16:28:36 +0200

    hw10_4

--- hw10_4.py
+++ hw10_4.py
@@ -7,7 +7,23 @@ with open('result.csv', 'r') as input_data, open('high_scores.csv', 'w') as outp
     next(input_data)
     data = csv.reader(input_data)
 
-    rows = [[row[0], int(row[1])] for row in data if row]
+    rows = [[row[0], int(row[1])] for row in data]
 
+    summary = {}
 
-print(rows)
+    for player in players:
+        summary[player] = 0
+
+    for k, v in summary.items():
+        for row in rows:
+            if row[0] == k:
+                if row[1] > v:
+                    summary[k] = row[1]
+
+    result = dict(sorted(summary.items(), key=lambda item: item[1], reverse=True))
+
+    writer = csv.writer(output_data)
+    writer.writerow(['Player name', 'Highest score'])
+
+    for key, value in result.items():
+        writer.writerow([key, value])
