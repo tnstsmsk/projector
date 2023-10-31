c547cfeeab3bd9adce2751e766fd98a955982eb8
tnstsmsk <omelkaramelka@gmail.com>
2023-10-31 13:32:49 +0200

    hw10_3

--- hw10_3.py
+++ hw10_3.py
@@ -1,20 +1,17 @@
 import random, csv
 
 players = ['Josh', 'Kate', 'Anna', 'Mary', 'Mark']
-result = {}
+result = []
 
 for player in players:
-    result[player] = 0
 
     for numb in range(100):
         score = random.randint(0, 10)
-        result[player] += score
-
-summary = [[key, value] for key, value in result.items()]
+        result.append([player, score])
 
 with open('result.csv', 'w') as file:
     writer = csv.writer(file)
     writer.writerow(['Player name', 'Score'])
 
-    for players in summary:
+    for players in result:
         writer.writerow(players)
