09f3e9689890f3da2dda3372cda5c998402c84eb
tnstsmsk <omelkaramelka@gmail.com>
2023-11-14 15:37:36 +0200

    less7

--- less7_2.py
+++ less7_2.py
@@ -11,8 +11,7 @@ def get_weather(api_key, city):
     params = {
         "q": city,
         "appid": api_key,
-        "units": "metric",  # You can change this to "imperial" for Fahrenheit
-    }
+        "units": "metric"}
 
     try:
         response = requests.get(base_url, params=params)
